793abf97cf4afe312d256b4a5d9c530ec7807df4
Harshit Agarwal <harshitagar1907@gmail.com>
2019-10-17 09:08:29 +0530

    maximize window

--- index.js
+++ index.js
@@ -13,12 +13,14 @@ function createWindow() {
     //   nodeIntegration: true
     // }
   })
-
+  
+  win.maximize();
   // and load the index.html of the app.
   win.loadFile('index.html')
 
+	
   // Open the DevTools.
-  win.webContents.openDevTools()
+  //win.webContents.openDevTools()
 
   // Emitted when the window is closed.
   win.on('closed', () => {
