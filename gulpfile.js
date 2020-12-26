49ae529b95b483fb32aba530626eb8bdc3882b30
Rajkumar S <rajkumaar2304@icloud.com>
2020-12-26 11:21:05 +0530

    add gulp uglify plugins
    - separate functions.css and main.css

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
 const gulp = require('gulp')
 const concat = require('gulp-concat')
 const nodemon = require('gulp-nodemon')
+const uglify = require('gulp-uglify-es').default
+const uglifyCss = require('gulp-uglifycss')
 let watchTask, nodemonTask
 
 function stopScripts() {
@@ -37,20 +39,49 @@ let functionVendor = [
 
 let functionMain = ['js/swal.js', 'js/events.js', 'js/functions.js']
 
+let mainCss = [
+    'css/vendor/bootstrap.min.css',
+    'css/vendor/joint.css',
+    'css/main.css',
+    'css/chat.css',
+]
+let functionCss = ['css/vendor/bootstrap.min.css', 'css/functions.css']
+
 gulp.task('concat', function () {
     gulp.src(indexVendor)
         .pipe(concat('indexVendor.js'))
+        .pipe(uglify({ keep_fnames: true }))
         .pipe(gulp.dest('js/build'))
-    gulp.src(indexMain).pipe(concat('indexMain.js')).pipe(gulp.dest('js/build'))
+    gulp.src(indexMain)
+        .pipe(concat('indexMain.js'))
+        .pipe(uglify({ keep_fnames: true }))
+        .pipe(gulp.dest('js/build'))
     gulp.src(functionVendor)
         .pipe(concat('functionVendor.js'))
+        .pipe(uglify({ keep_fnames: true }))
         .pipe(gulp.dest('js/build'))
     gulp.src(functionMain)
         .pipe(concat('functionMain.js'))
+        .pipe(uglify({ keep_fnames: true }))
         .pipe(gulp.dest('js/build'))
-    return gulp
-        .src('css/**/*.css')
+    gulp.src(mainCss)
         .pipe(concat('main.css'))
+        .pipe(
+            uglifyCss({
+                maxLineLen: 80,
+                uglyComments: true,
+            })
+        )
+        .pipe(gulp.dest('css/build'))
+    return gulp
+        .src(functionCss)
+        .pipe(concat('functions.css'))
+        .pipe(
+            uglifyCss({
+                maxLineLen: 80,
+                uglyComments: true,
+            })
+        )
         .pipe(gulp.dest('css/build'))
 })
 
