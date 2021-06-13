8ca68aaf01ae43c87be30a1b1752a8d42c4a8910
rajkumaar23 <rajkumaar23@users.noreply.github.com>
2021-06-13 09:06:12 +0000

    deploy: c9681f1ccff861afd1c20ba341cdc4e51ce2946a

--- sw.js
+++ sw.js
@@ -17,7 +17,7 @@
  # along with Flowgramming.  If not, see <http://www.gnu.org/licenses/>.
  # *************************************************************************************
  */
-const latestCacheName = 'flowgramming-v1623574499'
+const latestCacheName = 'flowgramming-v1623575171'
 
 /* Start the service worker and cache all of the app's content */
 self.addEventListener('install', function (e) {
