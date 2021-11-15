08a68481279868667526e56770bb7384bf415a9e
Tvashta <Tvashta@users.noreply.github.com>
2021-11-15 07:47:05 +0000

    deploy: c027d6684dd3f7e93a4d59dec4c43836d6b998fc

--- sw.js
+++ sw.js
@@ -18,7 +18,7 @@
  # *************************************************************************************
  */
 
-const latestCacheName = 'flowgramming-v1634875154'
+const latestCacheName = 'flowgramming-v1636962423'
 
 /* Start the service worker and cache all of the app's content */
 self.addEventListener('install', function (e) {
